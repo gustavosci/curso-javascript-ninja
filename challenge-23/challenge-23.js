05750ba3a88cc5660e0835c7d8fc14b8dc96225b
gustavosci <35278404+gustavosci@users.noreply.github.com>
2018-02-17 23:57:27 -0300

    Update challenge-23.js

--- challenge-23/challenge-23.js
+++ challenge-23/challenge-23.js
@@ -1,3 +1,5 @@
+(function(doc){
+
 /*
 Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
 As regras são:
@@ -23,3 +25,156 @@ multiplicação (x), então no input deve aparecer "1+2x".
 input;
 - Ao pressionar o botão "CE", o input deve ficar zerado.
 */
+
+	"use strict";
+
+	// Busca elementos da interface
+	var $iptCalculo = doc.querySelector( '[data-js="iptCalculo"]');
+	var $btn9 = doc.querySelector( '[data-js="btn9"]');
+	var $btn8 = doc.querySelector( '[data-js="btn8"]');
+	var $btn7 = doc.querySelector( '[data-js="btn7"]');
+	var $btn6 = doc.querySelector( '[data-js="btn6"]');
+	var $btn5 = doc.querySelector( '[data-js="btn5"]');
+	var $btn4 = doc.querySelector( '[data-js="btn4"]');
+	var $btn3 = doc.querySelector( '[data-js="btn3"]');	
+	var $btn2 = doc.querySelector( '[data-js="btn2"]');	
+	var $btn1 = doc.querySelector( '[data-js="btn1"]');	
+	var $btn0 = doc.querySelector( '[data-js="btn0"]');			
+	var $btnDivide = doc.querySelector( '[data-js="btnDivide"]');			
+	var $btnMultiplication = doc.querySelector( '[data-js="btnMultiplication"]');			
+	var $btnAdd = doc.querySelector( '[data-js="btnAdd"]');			
+	var $btnSubtraction = doc.querySelector( '[data-js="btnSubtraction"]');			
+	var $btnCE = doc.querySelector( '[data-js="btnCE"]');
+	var $btnResult =  doc.querySelector( '[data-js="btnResult"]');
+
+	// Desabilita e inicializa campo de cálculo
+	$iptCalculo.disabled = true;
+
+
+	// Eventos de clique nos botões
+	$btn9.addEventListener("click", function(eve){
+		eve.preventDefault();
+		$iptCalculo.value = $iptCalculo.value + "9";		
+	}, false);
+
+	$btn8.addEventListener("click", function(eve){
+		eve.preventDefault();
+		$iptCalculo.value = $iptCalculo.value + "8";		
+	}, false);
+	$btn7.addEventListener("click", function(eve){
+		eve.preventDefault();
+		$iptCalculo.value = $iptCalculo.value + "7";		
+	}, false);
+	$btn6.addEventListener("click", function(eve){
+		eve.preventDefault();
+		$iptCalculo.value = $iptCalculo.value + "6";		
+	}, false);
+	$btn5.addEventListener("click", function(eve){
+		eve.preventDefault();
+		$iptCalculo.value = $iptCalculo.value + "5";		
+	}, false);
+	$btn4.addEventListener("click", function(eve){
+		eve.preventDefault();
+		$iptCalculo.value = $iptCalculo.value + "4";		
+	}, false);
+	$btn3.addEventListener("click", function(eve){
+		eve.preventDefault();
+		$iptCalculo.value = $iptCalculo.value + "3";		
+	}, false);
+	$btn2.addEventListener("click", function(eve){
+		eve.preventDefault();
+		$iptCalculo.value = $iptCalculo.value + "2";		
+	}, false);
+	$btn1.addEventListener("click", function(eve){
+		eve.preventDefault();
+		$iptCalculo.value = $iptCalculo.value + "1";		
+	}, false);
+	$btn0.addEventListener("click", function(eve){
+		eve.preventDefault();
+		$iptCalculo.value = $iptCalculo.value + "0";		
+	}, false);
+	$btnDivide.addEventListener("click", function(eve){
+		eve.preventDefault();
+		addOperacao("/");
+	}, false);
+	$btnMultiplication.addEventListener("click", function(eve){
+		eve.preventDefault();
+		addOperacao("*");
+	}, false);
+	$btnAdd.addEventListener("click", function(eve){
+		eve.preventDefault();
+		addOperacao("+");
+	}, false);
+	$btnSubtraction.addEventListener("click", function(eve){
+		eve.preventDefault();	
+		addOperacao("-");
+	}, false);
+	$btnCE.addEventListener("click", function(eve){
+		eve.preventDefault();
+		$iptCalculo.value = "";
+	}, false);
+	$btnResult.addEventListener("click", function(eve){
+		eve.preventDefault();
+		exeCalc();
+	}, false);
+
+
+	// Subrotinas
+	function addOperacao(ope){
+		var strCalculo = $iptCalculo.value;
+		strCalculo = strCalculo.split("");
+		if(! isOperation(strCalculo[strCalculo.length - 1])){
+			strCalculo.push(ope);
+		} else {
+			strCalculo[strCalculo.length - 1] = ope;
+		}
+		$iptCalculo.value = strCalculo.join("");
+	}
+
+	function exeCalc(){
+		var result = 0;
+		var ope = "";
+		var numOpe = null;
+		var regexCalc = /(\d+)|[\/*\+-]/g;
+		var calculo = $iptCalculo.value.match(regexCalc);
+		calculo.forEach(function (valorAtual, index){
+			if(! isOperation(valorAtual)){
+				if(index === 0){
+					result = +valorAtual;
+				} else {
+					 numOpe = +valorAtual;
+				}
+			} else {
+				ope = valorAtual;
+			}
+			
+			if(ope !== "" && numOpe !== null){
+				switch(ope){
+					case "/":
+						result = result / numOpe;
+						break;
+					case "*":
+						result = result * numOpe;
+						break;
+					case "+":
+						result = result + numOpe;
+						break;
+					case "-":
+						result = result - numOpe;
+						break;
+				}						
+				ope = "";
+				numOpe = null;
+			}
+		})
+		$iptCalculo.value = result;
+	}
+
+	function isOperation(ope){
+		if(ope === "/" || ope === "*" || ope === "+" || ope === "-"){
+			return true;
+		}
+		return false;
+	}
+
+})(document);
