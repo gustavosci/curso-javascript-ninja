afbd853e2a1abe1853da6c26a2c4b64a6e7ef9e3
gustavosci <35278404+gustavosci@users.noreply.github.com>
2018-02-20 18:57:42 -0300

    Update challenge-25.js

--- challenge-25/challenge-25.js
+++ challenge-25/challenge-25.js
@@ -14,3 +14,70 @@ https://developer.mozilla.org/en-US/docs/Web/Events#Categories
 Tente aplicar na prática alguns dos eventos que estão ali e coloque nesse
 desafio os experimentos legais que você conseguir desenvolver :D
 */
+
+(function(doc){
+	"use strict";
+
+
+	// Eventos de teclado
+	var $iptTesteKeyDown = doc.querySelector('[data-js="iptTesteKeyDown"');
+	var $iptTesteKeyPress = doc.querySelector('[data-js="iptTesteKeyPress"');
+	var $iptTesteKeyUp = doc.querySelector('[data-js="iptTesteKeyUp"');	
+
+	$iptTesteKeyDown.addEventListener("keydown", handleTesteKeyDown, false);
+	$iptTesteKeyPress.addEventListener("keypress", handleTesteKeyPress, false);
+	$iptTesteKeyUp.addEventListener("keyup", handleTesteKeyUp, false);
+
+	function handleTesteKeyDown(eve){
+		$iptTesteKeyDown.value = $iptTesteKeyDown.value.toUpperCase();
+	}
+	function handleTesteKeyPress(eve){
+		$iptTesteKeyPress.value = $iptTesteKeyPress.value.toUpperCase();
+	}
+	function handleTesteKeyUp(eve){
+		$iptTesteKeyUp.value = $iptTesteKeyUp.value.toUpperCase();
+	}
+
+	// Clique com o botão direito do mouse
+	doc.addEventListener("contextmenu", handleContextMenu, false);
+	function handleContextMenu(eve){
+		console.log("Clicou com o botão direito do mouse");
+	}
+
+	// Selecionou texto de input ou textarea
+	doc.addEventListener("select", handleSelect, false);
+	function handleSelect(eve){
+		console.log("Selecionou algum texto");
+	}
+
+	// Copiou algum texto
+	doc.addEventListener("copy", handleCopy, false);
+	function handleCopy(eve){
+		console.log("Copiou algo");
+	}
+
+	// Colou algum texto
+	doc.addEventListener("paste", handlePaste, false);
+	function handlePaste(eve){
+		console.log("Colou algo");
+	}
+
+
+	// Copiar texto do primeiro input para clipboard
+	var $btnCopyInp = doc.querySelector('[data-js="btnCopyInp"');	
+	$btnCopyInp.addEventListener("click", handleCopyInp, false);
+	function handleCopyInp(eve){
+		$iptTesteKeyDown.select();
+		doc.execCommand("copy");
+	}
+
+	// Cola texto clipboard para primeiro input 
+	var $btnPasteInp = doc.querySelector('[data-js="btnPasteInp"');	
+	$btnPasteInp.addEventListener("click", handlePasteInp, false);
+	function handlePasteInp(eve){
+		alert("Não rolou :(")
+	}
+
+
+
+})(document);
